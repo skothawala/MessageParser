0cbf7d941845ab35735bb4f7ce67ad9a18154237
Saad Kothawala <skothawala@luc.edu>
2016-01-30 14:52:04 -0600

    isUrl and isUrl test

--- TextUtils.js
+++ TextUtils.js
@@ -15,6 +15,48 @@ var TextUtils = {
 	**/
 	getIndividualComponents: function (fullText){
 		return fullText.split(" ");
+	},
+	/**
+	 * Checks if the input is a url
+	 * 
+	 * @param text -> the url?
+	 * @return -> boolean
+	 * Example:
+	 *	isUrl('http://www.tpgr.me/XJ05d') -> true;
+	**/
+	isUrl: function(text){
+		if(text.charAt(0) == '@' || text.charAt(0) == '#')
+			return false;
+
+		//regex from Android http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/2.0_r1/android/text/util/Regex.java#Regex.0WEB_URL_PATTERN
+		var isUrl = text.match(/((?:(http|https|Http|Https|rtsp|Rtsp):\/\/(?:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,64}(?:\:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,25})?\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\:\d{1,5})?)(\/(?:(?:[a-zA-Z0-9\;\/\?\:\@\&\=\#\~\-\.\+\!\*\'\(\)\,\_])|(?:\%[a-fA-F0-9]{2}))*)?(?:\b|$)/gi);
+		
+		return isUrl != null;
+	},
+
+	/**
+	 * This is the "main" function of this class. This is what the user calls from outside. Basically,
+	 * we take a tweet and then process it and return the tweet as an array of json objects containing it's classification
+	 * 
+	 * 
+	 * @param tweet -> the tweet
+	 * @return -> array
+	 * Example:
+	 *	processTweet('@saad hello @john, visit http://google.com #awesome') -> 
+	 *		[{text: '@saad', type: 'mention'}, 
+	 		{text: 'hello', type: 'text'}, 
+	 		{text: '@john', type: 'mention'}, 
+	 		{text: 'visit', type: 'text'}, 
+	 		{text: 'http://google.com', type: 'url'},	 		
+	 		{text: '#awesome', type: 'mention'}];
+	**/
+	processTweet: function (tweet){
+		//1st we split the string by spaces
+
+		var components = TextUtils.getIndividualComponents(tweet);
+		for (var i = 0; i < components.length; i++) {
+			components[i]
+		};
 	}
 }
 
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var TextUtils = require('./TextUtils.js');
 
 var htmlHead = '<!DOCTYPE html><html lang="en"><head><!-- view-source:https://ironsummitmedia.github.io/startbootstrap-bare/ --> <meta charset="utf-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1"> <meta name="description" content=""> <meta name="author" content=""> <title>Tweet Processor</title> <!-- Bootstrap Core CSS --> <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet"> <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries --> <!-- WARNING: Respond.js doesn\'t work if you view the page via file:// --> <!--[if lt IE 9]> <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script> <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script> <![endif]--></head><body> <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation"> <div class="container"> <div class="navbar-header"> <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a class="navbar-brand" href="/">Tweet Processor</a> </div><div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"> <ul class="nav navbar-nav"> <li> <a href="/">Home</a> </li></ul> </div></div></nav><div class="container" style="padding-top:100px;">';
 var innerHtml = '<h1>Hello</h1>';
@@ -16,6 +17,9 @@ requestHandler = function (req, res) {
 
 var port = process.env.PORT || 8080;//for heroku (in future)
 var server = http.createServer(requestHandler);
-server.listen(port);
+//server.listen(port);
 
-console.log('Listening on ' + port);
+//console.log('Listening on ' + port);
+
+
+console.log(TextUtils.isUrl('http://google.com'));
