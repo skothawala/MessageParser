950d905259a0e02a26ba401b838bc9af10b9ad7a
Saad Kothawala <skothawala@luc.edu>
2016-01-30 16:07:06 -0600

    Started form

--- server.js
+++ server.js
@@ -1,22 +1,73 @@
 var http = require('http');
+var formidable = require('formidable');
 var TextUtils = require('./TextUtils.js');
 
 var htmlHead = '<!DOCTYPE html><html lang="en"><head><!-- view-source:https://ironsummitmedia.github.io/startbootstrap-bare/ --> <meta charset="utf-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1"> <meta name="description" content=""> <meta name="author" content=""> <title>Tweet Processor</title> <!-- Bootstrap Core CSS --> <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet"> <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries --> <!-- WARNING: Respond.js doesn\'t work if you view the page via file:// --> <!--[if lt IE 9]> <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script> <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script> <![endif]--></head><body> <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation"> <div class="container"> <div class="navbar-header"> <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a class="navbar-brand" href="/">Tweet Processor</a> </div><div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"> <ul class="nav navbar-nav"> <li> <a href="/">Home</a> </li></ul> </div></div></nav><div class="container" style="padding-top:100px;">';
-var innerHtml = '<h1>Hello</h1>';
+var innerHtml = '<p>This does blah and blah</p><div class="row"><form action="/analyze" method="post" enctype="multipart/form-data"><div class="col-md-10"><textarea style="resize:vertical;" rows="10" class="form-control" placeholder="Enter each tweet on a new line" name="tweets" value=""></textarea></div><div class="col-md-2"><input type="submit" class="btn btn-success" value="Analyze"></div></form></div>';
 var htmlFoot = '</div><!-- jQuery Version 1.11.1 --> <script src="https://code.jquery.com/jquery-2.2.0.min.js"></script> <!-- Bootstrap Core JavaScript --> <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script></body></html>';
 
+//handles errors
+handleError = function (err, res){
+	res.writeHead(200, {'content-type': 'text/html'});
+	res.write(htmlHead);
+	res.write('<span style="color:red; font-size:18px">Error: ' + err + "</span>");
+	res.write(innerHtml);
+	res.end(htmlFoot);
+}
+
+//handles successfully posted
+handleSuccess = function (data, mostCommon, res){
+	res.writeHead(200, {'content-type': 'text/html'});
+	res.write(htmlHead);
+	res.write('<p>Input: </p><pre>' + data + '</pre>');
+	res.write('<table class="table table-hover"><thead><tr><th>Tweet</th><th>Processed</th></tr></thead><tbody>')
+
+	//because I'm not using a min heap or a sorted structure, I need to sort the most common
+	var sorted = [];
+	for(word in mostCommon)
+		sorted.push([word, mostCommon[word]]);
+	sorted.sort(function(a, b) {return b[1] - a[1]})
+
+	
+	for (var i = 0; i < sorted.length; i++) {
+		res.write('<tr>');
+			res.write('<td>');
+			res.write(String(sorted[i][0]));//res.write needs to be string
+			res.write('</td>');
+			res.write('<td>');
+			res.write(String([sorted[i][1]]));//res.write needs to be string
+			res.write('</td>');
+		res.write('</tr>');
+	};
+	res.write('</tbody></table>');
+	res.write(htmlForm);
+	res.end(htmlFoot);
+
+}
 
 //handles all requests
 requestHandler = function (req, res) {
- 
-	res.writeHead(200, {'content-type': 'text/html'});
-	res.end(htmlHead + innerHtml + htmlFoot);
+	
+	if (req.url === '/analyze' && req.method.toLowerCase() === 'post') {
+		var form = new formidable.IncomingForm();
+
+		form.parse(req, function(err, fields, files) {
+			if(fields.length == 0 || fields.tweets.length == 0)
+				return handleError('Nothing inputed', res);
+		});
+
+	}else{
+		res.writeHead(200, {'content-type': 'text/html'});
+		res.end(htmlHead + innerHtml + htmlFoot);
+	}
+
+	
 
 };
 
 
 var port = process.env.PORT || 8080;//for heroku (in future)
 var server = http.createServer(requestHandler);
-//server.listen(port);
+server.listen(port);
 
-//console.log('Listening on ' + port);
+console.log('Listening on ' + port);
